b6015565ba0528bf66549dbdf0857e32a4c8a418
Umjh <42991076+Umjh@users.noreply.github.com>
2018-09-10 10:38:26 +0900

    Update dd.c

--- dd.c
+++ dd.c
@@ -2,22 +2,7 @@
 
 int main() {
   
-  int #;
-  int #;
-  float #;
-  float #;
-  
-  printf("정수 a 입력 : ");
-  scanf("#", #);
-  printf("입력받은 정수 a : # \n", #);
-  
-  printf("실수 c 입력 : ");
-  scanf("#", #);
-  printf("입력받은 실수 c : # \n", #);
-  
-  printf("정수 b 와 실수 d 입력 : ");
-  scanf("# #", #, #);
-  printf("입력받은 b d : # # \n", #, #);
+  printf("oo");
   
   return 0;
 }
