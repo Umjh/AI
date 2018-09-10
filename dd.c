123b33ae1683e00cdbd4e3296e86c37b0738d6eb
Umjh <42991076+Umjh@users.noreply.github.com>
2018-09-10 10:22:05 +0900

    Create dd.c

---
+++ dd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  
+  int #;
+  int #;
+  float #;
+  float #;
+  
+  printf("정수 a 입력 : ");
+  scanf("#", #);
+  printf("입력받은 정수 a : # \n", #);
+  
+  printf("실수 c 입력 : ");
+  scanf("#", #);
+  printf("입력받은 실수 c : # \n", #);
+  
+  printf("정수 b 와 실수 d 입력 : ");
+  scanf("# #", #, #);
+  printf("입력받은 b d : # # \n", #, #);
+  
+  return 0;
+}
